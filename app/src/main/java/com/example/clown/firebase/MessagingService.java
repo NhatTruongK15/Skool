309535fafb889d8101176dd30670e51fc2e60a24
lehquy13 <lehquy13@gmail.com>
2022-05-07 07:40:58 +0700

    fix received notification on Pixel 4 api31

--- app/src/main/java/com/example/clown/firebase/MessagingService.java
+++ app/src/main/java/com/example/clown/firebase/MessagingService.java
@@ -1,5 +1,8 @@
 package com.example.clown.firebase;
 
+import static android.app.PendingIntent.FLAG_IMMUTABLE;
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -25,7 +28,7 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-        Log.d("FCM","Token: " + token);
+        Log.d("FCM", "Token: " + token);
     }
 
     @Override
@@ -41,10 +44,18 @@ public class MessagingService extends FirebaseMessagingService {
         String channelId = "chat_message";
         Intent intent = new Intent(this, ChatActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.putExtra(Constants.KEY_USER,user);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent, 0);
+        intent.putExtra(Constants.KEY_USER, user);
+        int flags;
+        if
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            flags = FLAG_UPDATE_CURRENT | FLAG_IMMUTABLE;
+        } else
+            flags = FLAG_UPDATE_CURRENT;
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,channelId);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, flags);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId);
         builder.setSmallIcon(R.drawable.ic_notification);
         builder.setContentTitle(user.name);
         builder.setContentText(message.getData().get(Constants.KEY_MESSAGE));
@@ -55,12 +66,11 @@ public class MessagingService extends FirebaseMessagingService {
         builder.setContentIntent(pendingIntent);
         builder.setAutoCancel(true);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence channelName = "Chat message";
             String channelDescription = "This notification channel is used for chat message notifications";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(channelId,channelName,importance);
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
             channel.setDescription(channelDescription);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
